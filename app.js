d4937663a3020d91124d48e62e795bc21d4661bd
Ike <are@voyancehq.com>
2023-10-20 21:32:41 +0100

    add prisma

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-const me = 'ike';
+
+const { PrismaClient } = require('@prisma/client') 
+const prisma = new PrismaClient()
+
+const express = require('express')
+const app = express()
+
+
+//PROJECTS
+
+//Get projects
+app.get('/projects',  (req, res) => {
+    res.send('hi')
+})
+
+//Get a project
+
+//Create a project
+
+//Delete a project
+
+//Update a project 
+
+
+//EVENTS
+
+//Get events
+
+//Get an event
+
+
+//WORKFLOWS
+
+//Get a workflow
+
+//Create a workflow
+
+//Delete a workflow
+
+//Update a workflow 
+
+const port = process.env.PORT || 8000
+app.listen(port)
